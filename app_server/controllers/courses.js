1dc8be771822ca56defa5c7213907674f6eacbf6
RebeccaMay <remay@mines.edu>
2018-03-05 23:15:20 -0700

    all hard coded data moved to controllers. Unit 8 completed

--- app_server/controllers/courses.js
+++ app_server/controllers/courses.js
@@ -18,7 +18,31 @@ module.exports.homelist = function(req, res){
 };
 
 module.exports.courseInfo = function(req, res){
-	res.render('course-info', {title: 'My Courses'});
+	res.render('course-info', {
+		title: 'MATH-335 Course Info',
+		pageHeader: {title: 'MATH-335'},
+		courseInfo: {
+			courseId: 'MATH-335',
+			courseName: 'Introduction to Statistics',
+			professor: 'Ashlyn Munson',
+			times: 'MWF 9am, Alderson 162',
+			credits: '3'
+		},
+		assignments: [{
+			hwName: 'HW1',
+			hwDescription: 'Chapter 1',
+			dueDate: 'Jan 17, 2018',
+			points: '20',
+			hwStatus: 'Graded'
+		},{
+			hwName: 'HW2',
+			hwDescription: 'Chapter 2',
+			dueDate: 'Jan 24, 2018',
+			points: '18',
+			hwStatus: 'Completed'
+		}
+		]
+	});
 };
 
 module.exports.addHw = function(req, res){
