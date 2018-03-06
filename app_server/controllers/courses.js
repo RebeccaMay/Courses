bd34f85cd1c4aaf21586789279f343eaa24fbd50
RebeccaMay <remay@mines.edu>
2018-03-05 22:23:33 -0700

    moved homelist data to controller from view

--- app_server/controllers/courses.js
+++ app_server/controllers/courses.js
@@ -1,5 +1,20 @@
 module.exports.homelist = function(req, res){
-	res.render('courses-list', {title: 'Course Organizer'});
+	res.render('courses-list', {
+		title: 'Course Organizer - all your assignments in one place',
+		pageHeader: {
+			title: 'Course Organizer',
+			strapline: 'All your assignments in one place!'
+		},
+		courselist: [{
+			courseId: 'MATH-335',
+			professor: 'Ashlyn Munson',
+			courseName: 'Introduction to Statistics'
+		},{
+			courseId: 'CSCI-445',
+			professor: 'Cyndi Rader',
+			courseName: 'Web Applications'
+		}]
+	});
 };
 
 module.exports.courseInfo = function(req, res){
