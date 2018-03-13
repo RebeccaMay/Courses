587c05ee197b00b8e277f08e3b43833dde7d09e3
RebeccaMay <remay@mines.edu>
2018-03-13 12:04:59 -0600

    created schemas

--- app_server/models/courses.js
+++ app_server/models/courses.js
@@ -1,3 +1,24 @@
 var mongoose = require('mongoose');
 
-var courseSchema = new mongoose.Schema({ });
+var hwSchema = new mongoose.Schema({
+	hwName: {type: String, required: true},
+	hwDescription: String,
+	dueDate: {type: String, required: true},
+	points: {type: Number, min: 0, max: 100},
+	hwStatus: String
+});
+
+var courseTimeSchema = new mongoose.Schema({
+	times: String,
+	credits: Number
+});
+
+var courseSchema = new mongoose.Schema({ 
+	courseID: {type: String, required: true},
+	professor: String,
+	courseName: {type: String, required: true},
+	courseTime: [courseTimeSchema],
+	assignments: [hwSchema]
+});
+
+mongoose.model('Course', courseSchema);
