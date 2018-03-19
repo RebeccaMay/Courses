ef426c9b29ca0cf1f2cff29d3d95efe232681fbf
RebeccaMay <remay@mines.edu>
2018-03-19 00:00:11 -0600

    working on read one assignment, just edited db

--- app_api/controllers/assignments.js
+++ app_api/controllers/assignments.js
@@ -11,7 +11,49 @@ module.exports.assignmentsCreate = function (req, res) {
 };
 
 module.exports.assignmentsReadOne = function (req, res) {
-	sendJsonResponse(res, 200, {"status" : "success"});		
+	if (req.params && req.params.courseId) {
+		Cour
+			.findById(req.params.courseId)
+			.select('courseName assignments')
+			.exec(function(err, course) {
+				var response, assignment;
+				if (!course) {
+					sendJsonResponse(res, 404, {
+						"message": "course Id not found"
+					});
+					return;
+				} else if (err) {
+					sendJsonResponse(res, 404, err);
+					return;
+				}
+				if (course.assignments && course.assignments.length > 0) {
+					assignment = course.assignments.id(req.params.assignmentid);
+					if (!assignment) {
+						sendJsonResponse(res, 404, {
+							"message": "assignmentid not found"
+						});
+					} else {
+						response = {
+							course : {
+								name : course.courseName,
+								id : req.params.courseId
+							},
+							assignment : assignment
+						};
+						sendJsonResponse(res, 200, response);
+					}
+				} else {
+					sendJsonResponse(res, 404, {
+						"message": "No assignments found"
+					});
+				}
+				sendJsonResponse(res, 200, course);
+			});
+	} else {
+		sendJsonResponse(res, 404, {
+			"message": "No courseId in request"
+		});
+	}		
 };
 
 module.exports.assignmentsUpdateOne = function (req, res) {
--- app_api/controllers/courses.js
+++ app_api/controllers/courses.js
@@ -14,8 +14,27 @@ module.exports.coursesList = function (req, res) {
 	sendJsonResponse(res, 200, {"status" : "success"});		
 };
 
-module.exports.coursesReadOne = function (req, res) {
-	sendJsonResponse(res, 200, {"status" : "success"});		
+module.exports.coursesReadOne = function(req, res) {
+	if (req.params && req.params.courseId) {
+		Cour
+			.findById(req.params.courseId)
+			.exec(function(err, course) {
+				if (!course) {
+					sendJsonResponse(res, 404, {
+						"message": "courseId not found"
+					});
+					return;
+				} else if (err) {
+					sendJsonResponse(res, 404, err);
+					return;
+				}
+			sendJsonResponse(res, 200, course);
+			});
+	} else {
+		sendJsonResponse(res, 404, {
+			"message": "No courseId in request"
+		});
+	}
 };
 
 module.exports.coursesUpdateOne = function (req, res) {
